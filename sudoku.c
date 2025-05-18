f6adc7bbcfc0a3d25e4d3f2f4e8ffaba8b0a53bd
Naomi175 <naomi.alveal.m@mail.pucv.cl>
2025-05-17 22:07:39 -0400

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -83,7 +83,21 @@ int is_valid(Node* n){
 
 
 List* get_adj_nodes(Node* n){
-    List* list=createList();
+   List* list=createList();
+   
+   for (int i = 0 ; i < 9 ; i++) 
+    for (int j = 0 ; j < 9 ; j++) 
+       if (n->sudo[i][j] == 0) {
+        for (int k = 1 ; k <= 9 ; k++) {
+          Node* nuevoNodo = copy(n);
+          nuevoNodo->sudo[i][j] = k;
+          if (is_valid(nuevoNodo))
+            pushBack(list, nuevoNodo);
+          else 
+            free(nuevoNodo);
+        }
+        return list;
+       }
     return list;
 }
 
